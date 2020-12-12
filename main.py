cbaf77ccb70fe8f6b9eeacdea09f190b35f12de5
Jia Hao <71251813+jiah29@users.noreply.github.com>
2020-12-12 09:54:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from init_dataclass import init_countries
 from computation import filter_country, find_emission_average, find_average_revenue
 from display_graph import create_dataframe, plot_graph
 
+
 if __name__ == '__main__':
 
     #  Read data from csv and create dataclass objects for use in computations:
@@ -31,19 +32,19 @@ if __name__ == '__main__':
     countries = init_countries()
 
     #  Filter the countries list into four different mappings according to
-    #  their income classifications.
+    #  their income groups.
     high = filter_country(countries, 'High income')
     upper_middle = filter_country(countries, 'Upper middle income')
     lower_middle = filter_country(countries, 'Lower middle income')
     low = filter_country(countries, 'Low income')
 
-    #  Find the average emission value of each sector for each income classification.
+    #  Find the average emission value of each sector for each income group.
     high_average_emission = find_emission_average(high)
     upper_middle_average_emission = find_emission_average(upper_middle)
     lower_middle_average_emission = find_emission_average(lower_middle)
     low_average_emission = find_emission_average(low)
 
-    #  Find the average revenue of each sector for each income classification.
+    #  Find the average revenue of each sector for each income group.
     high_average_revenue = find_average_revenue(high)
     upper_middle_average_revenue = find_average_revenue(upper_middle)
     lower_middle_average_revenue = find_average_revenue(lower_middle)
@@ -66,7 +67,7 @@ if __name__ == '__main__':
     low_income.update(low_average_emission)
     low_income.update(low_average_revenue)
 
-    #  Creating dataframe objects
+    #  Create dataframe objects for each income group
     high_income_df = create_dataframe(high_income)
     upper_middle_income_df = create_dataframe(upper_middle_income)
     lower_middle_income_df = create_dataframe(lower_middle_income)
@@ -76,13 +77,13 @@ if __name__ == '__main__':
     plot_graph(high_income_df, 'High Income')
 
     #   If you want to see the graph for upper middle income countries,
-    #   uncomment the code below, and comment out other plot_graph functions.
+    #   uncomment the code below.
     # plot_graph(upper_middle_income_df, 'Upper Middle Income')
 
     #   If you want to see the graph for lower middle income countries,
-    #   uncomment the code below, and comment out other plot_graph functions.
+    #   uncomment the code below.
     # plot_graph(lower_middle_income_df, 'Lower Middle Income')
 
     #   If you want to see the graph for low income countries,
-    #   uncomment the code below, and comment out other plot_graph functions.
+    #   uncomment the code below.
     # plot_graph(low_income_df, 'Low Income')
